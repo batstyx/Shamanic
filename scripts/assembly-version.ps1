e756568cf4236f64ff87fd4ca8e0437d7ba415e6
Batian Styx <batstyx@gmail.com>
2021-07-04 18:58:09 +0100

    Update build-and-release-on-version-tag.yml

--- scripts/assembly-version.ps1
+++ scripts/assembly-version.ps1
@@ -3,23 +3,28 @@ Param(
     [string]$project,
     [Parameter(Mandatory=$true)]
     [int]$build,
-    [Parameter()]
-    [int]$overrideMajor,
-    [Parameter()]
-    [int]$overrideMinor,
-    [Parameter()]
-    [int]$overridePatch
+    [Parameter(Mandatory=$true)]
+    [string]$overrideVersion
 )
+$versionPattern = '(?<major>\d+)\.(?<minor>\d+)(\.(?<patch>\d+))?(\.(?<build>\d+))?'
 
 $solutionPath = $(Resolve-Path "$PSScriptRoot\..").Path
 $assemblyInfoFile = "$solutionPath\$project\Properties\AssemblyInfo.cs"
 
 $assemblyInfo = [IO.File]::ReadAllText($assemblyInfoFile)
-$versionRegex = [System.Text.RegularExpressions.Regex]::new('Version\(\"(?<major>\d+)\.(?<minor>\d+)(\.(?<patch>\d+))?(\.(?<build>\d+))?\"\)')
+$versionRegex = [System.Text.RegularExpressions.Regex]::new('Version\(\"' + $versionPattern + '\"\)')
 
 $match = $versionRegex.Match($assemblyInfo)
 if(!$match.Success) {
     throw "No version numbers formatted <major>.<minor>(.<patch>(.<build>)) found in $assemblyInfoFile"
+}
+if($overrideVersion)
+{
+    $overrideRegex = [System.Text.RegularExpressions.Regex]::new('v' + $versionPattern)
+    $override = $overrideRegex.Match($overrideVersion)
+    if($override.Groups['major'].Success) {$overrideMajor=$override.Groups['major'].Value}
+    if($override.Groups['minor'].Success) {$overrideMinor=$override.Groups['minor'].Value}
+    if($override.Groups['patch'].Success) {$overridePatch=$override.Groups['patch'].Value}
 }
 $major = if($overrideMajor) {$overrideMajor} else {$match.Groups['major'].Value}
 $minor = if($overrideMinor) {$overrideMinor} else {$match.Groups['minor'].Value}
