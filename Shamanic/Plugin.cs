46116feade6006047cc01132ef376bc27dea6431
Batian Styx <batstyx@gmail.com>
2021-11-25 09:19:18 +0000

    Improved Plugin behaviour - Only save settings on unload when previously loaded - Link EventManager to API events - Plugin menu item to settings

--- Shamanic/Plugin.cs
+++ Shamanic/Plugin.cs
@@ -1,37 +1,57 @@
-﻿using Hearthstone_Deck_Tracker;
-using Hearthstone_Deck_Tracker.API;
+﻿using Hearthstone_Deck_Tracker.API;
 using Hearthstone_Deck_Tracker.Plugins;
 using Shamanic.Properties;
 using Shamanic.Views;
 using System;
-using System.Diagnostics;
-using System.Windows;
 using System.Windows.Controls;
-using static Shamanic.Views.EffectView;
-using CoreAPI = Hearthstone_Deck_Tracker.API.Core;
 
 namespace Shamanic
 {
     public class Plugin : IPlugin
     {
+        private Settings Settings;
+        internal static EventManager Events { get; private set; }
+
         public string Name => LibraryInfo.Name;
         public string Description => Strings.Get("PluginDescription");
         public string ButtonText => Strings.Get("PluginButtonText");
         public string Author => "batstyx";
         public Version Version => LibraryInfo.Version;
-        public MenuItem MenuItem => null;
+        public MenuItem MenuItem { private set; get; }
 
         public void OnButtonPress() => SettingsView.Flyout.IsOpen = true;
 
         private Shamanic Shamanic;
 
-        public void OnLoad() => Shamanic = new Shamanic();
+        public void OnLoad()
+        {
+            Settings = Settings.Default;
+
+            MenuItem = new MenuItem { Header = Name };
+            MenuItem.Click += (sender, args) => OnButtonPress();
+
+            Events = new EventManager();
+
+            GameEvents.OnGameStart.Add(Events.OnGameStart);
+            GameEvents.OnInMenu.Add(Events.OnInMenu);
+
+            GameEvents.OnPlayerPlay.Add(Events.OnPlayerPlay);
+            GameEvents.OnPlayerCreateInPlay.Add(Events.OnPlayerCreateInPlay);
+
+            GameEvents.OnOpponentPlay.Add(Events.OnOpponentPlay);
+            GameEvents.OnOpponentCreateInPlay.Add(Events.OnOpponentCreateInPlay);
+
+            Shamanic = new Shamanic();
+        }
 
         public void OnUpdate() => Shamanic.Refresh();
 
         public void OnUnload()
         {
-            if (Settings.Default.HasChanges) Settings.Default.Save();
+            if (Settings?.HasChanges ?? false) Settings.Save();
+
+            Events?.Dispose();
+            Events = null;
 
             Shamanic?.Dispose();
             Shamanic = null;
