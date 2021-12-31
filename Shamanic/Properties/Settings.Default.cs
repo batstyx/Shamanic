52906db6e1d6d69540b7e9fe12f648247e961c5d
Batian Styx <batstyx@gmail.com>
2021-12-31 17:33:52 +0000

    Separate file for default Settings code

---
+++ Shamanic/Properties/Settings.Default.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shamanic.Properties
+{
+    public sealed partial class Settings
+    {
+        private int GetDefaultIntValue(string key) => int.Parse(Properties[key].DefaultValue.ToString());
+        public int DefaultPlayerLeft => GetDefaultIntValue("PlayerLeft");
+        public int DefaultPlayerTop => GetDefaultIntValue("PlayerTop");
+        public int DefaultOpponentLeft => GetDefaultIntValue("OpponentLeft");
+        public int DefaultOpponentTop => GetDefaultIntValue("OpponentTop");
+        public void ResetPlayerPosition()
+        {
+            PlayerLeft = DefaultPlayerLeft;
+            PlayerTop = DefaultPlayerTop;
+        }
+        public void ResetOpponentPosition()
+        {
+            OpponentLeft = DefaultOpponentLeft;
+            OpponentTop = DefaultOpponentTop;
+        }
+    }
+}
--- Shamanic/Properties/Settings.cs
+++ Shamanic/Properties/Settings.cs
@@ -22,22 +22,6 @@ namespace Shamanic.Properties
 
     public sealed partial class Settings
     {
-        private int GetDefaultIntValue(string key) => int.Parse(Properties[key].DefaultValue.ToString());
-        public int DefaultPlayerLeft => GetDefaultIntValue("PlayerLeft");
-        public int DefaultPlayerTop => GetDefaultIntValue("PlayerTop");
-        public int DefaultOpponentLeft => GetDefaultIntValue("OpponentLeft");
-        public int DefaultOpponentTop => GetDefaultIntValue("OpponentTop");
-        public void ResetPlayerPosition()
-        {
-            PlayerLeft = DefaultPlayerLeft;
-            PlayerTop = DefaultPlayerTop;
-        }
-        public void ResetOpponentPosition()
-        {
-            OpponentLeft = DefaultOpponentLeft;
-            OpponentTop = DefaultOpponentTop;
-        }
-
         private static readonly string Filename = Path.ChangeExtension(LibraryInfo.Name, ".xml");
         internal static string DataDir => Config.Instance.DataDir;
         private static string SettingsPath => Path.Combine(DataDir, Filename);
@@ -77,7 +61,7 @@ namespace Shamanic.Properties
                             Log.Error(ex);
                         }
                     }                   
-                    Log.Info($"{SettingsPath} Loaded");
+                    Log.Info($"{SettingsPath} loaded");
                 }
                 else
                 {
